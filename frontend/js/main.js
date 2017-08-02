4dfd61bed7497520323f8f564a14f28006096b71
Anthony Ngene <anthony.ngene@andela.com>
2017-08-02 13:28:00 +0100

    feature: build section 2 - mission and award

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
   $(".button-collapse").sideNav();
+  
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    navText: ['<i class="large material-icons">chevron_left</i>', '<i class="large material-icons">chevron_right</i>'],
+    responsive:{
+      0:{
+        items:1
+      },
+      600:{
+        items:3
+      },
+      1000:{
+        items:4
+      }
+    }
+  })
 });
