cb936f8496d940b220b0656bc1554ac9dfe07a86
Anthony Ngene <anthony.ngene@andela.com>
2017-08-04 06:49:52 +0100

    feature(homepage): build the testimonial section and the footer

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp'),
-      scss = require('gulp-sass');
+      scss = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scss', () => {
   gulp.src('scss/*.scss')
   .pipe(scss({outputStyle: 'compressed'}).on('error', scss.logError))
+  .pipe(autoprefixer())
   .pipe(gulp.dest('css'));
 });
 
