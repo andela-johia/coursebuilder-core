0de2df044711526b22dd70f9263c48bae9dacaef
Anthony Ngene <anthony.ngene@andela.com>
2017-08-13 08:16:31 +0100

    chore(main.js): properly indent the javascript file

--- coursebuilder/modules/explorer/_static/js/main.js
+++ coursebuilder/modules/explorer/_static/js/main.js
@@ -1,50 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $(".button-collapse").sideNav();
-  
+
   $('.awards-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
+    loop: true,
+    margin: 10,
+    nav: true,
     dots: false,
     navText: ['<i class="ion-ios-arrow-left"></i>', '<i class="ion-ios-arrow-right"></i>'],
-    responsive:{
-      0:{
-        items:1
+    responsive: {
+      0: {
+        items: 1
       },
-      600:{
-        items:2
+      600: {
+        items: 2
       },
-      1000:{
-        items:4
+      1000: {
+        items: 4
       }
     }
   });
-  
+
   $('.how-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
+    loop: true,
+    margin: 10,
+    nav: true,
     navText: ['<i class="ion-ios-arrow-left"></i>', '<i class="ion-ios-arrow-right"></i>'],
     items: 1
   });
 
 
-    function showPage(pageName) {
-        $('.page-wrapper').hide();
-        $('#' + pageName).show();
-    }
+  function showPage(pageName) {
+    $('.page-wrapper').hide();
+    $('#' + pageName).show();
+  }
 
-    var pageLinks = ['explorer', 'certification', 'homepage', 'tools', 'partners', 'feedback'];
+  var pageLinks = ['explorer', 'certification', 'homepage', 'tools', 'partners', 'feedback'];
 
-    pageLinks.forEach(function (name) {
-        $('a[href="#!' + name + '"]').on('click', function () {
-            showPage(name);
-        });
-
-        if (window.location.href.substr(-name.length) === name) {
-            showPage(name);
-        }
+  pageLinks.forEach(function (name) {
+    $('a[href="#!' + name + '"]').on('click', function () {
+      showPage(name);
     });
 
-    $('select').material_select();
+    if (window.location.href.substr(-name.length) === name) {
+      showPage(name);
+    }
+  });
+
+  $('select').material_select();
 });
