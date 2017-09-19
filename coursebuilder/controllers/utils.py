1019f258b655fd48e500847a0127eaa6fd53d147
Anthony Ngene <anthony.ngene@andela.com>
2017-09-19 16:56:39 +0100

    fix(carousel): ensure that the carousel renders if the keywords are within html tags

--- coursebuilder/controllers/utils.py
+++ coursebuilder/controllers/utils.py
@@ -30,6 +30,7 @@ import jinja2
 import sites
 import webapp2
 from webob import multidict
+from lxml import html
 
 import appengine_config
 from common import jinja_utils
@@ -603,13 +604,18 @@ class ApplicationHandler(webapp2.RequestHandler):
         try:
             template = self.get_template(template_file, additional_dirs)
             content = template.render(template_values, autoescape=True)
-            carousel_markup = {
-                '[start_carousel]': '<div class="owl-carousel owl-theme course-carousel"><div class="item">',
-                '[split]': '</div><div class="item">',
-                '[stop_carousel]': '</div></div>'
-            }
-            for keyword, markup in carousel_markup.iteritems():
-                content = content.replace(keyword, markup)
+            try:
+                start_key = '[start_carousel]'
+                stop_key = '[stop_carousel]'
+                start = content.index(start_key) + len(start_key)
+                end = content.index(stop_key)
+                to_be_formatted = content[start:end].split('[split]')
+                to_be_formatted = '<div class="owl-carousel owl-theme course-carousel"><div class="item">' + '</div><div class="item">'.join(
+                    map(lambda item: html.tostring(html.fromstring(item)),
+                        to_be_formatted)) + '</div></div>'
+                content = content[:start - len(start_key)] + to_be_formatted + content[end + len(stop_key):]
+            except ValueError:
+               pass
             return jinja2.utils.Markup(content)
         finally:
             models.MemcacheManager.end_readonly()
